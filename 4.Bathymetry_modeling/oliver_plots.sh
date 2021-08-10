2d5d5a5e14798082b63274f2489f3dbacda494ac
bhyleee <bhyleee@gmail.com>
2021-08-10 14:38:06 -0700

    oliver_ploits

--- 4.Bathymetry_modeling/oliver_plots.sh
+++ 4.Bathymetry_modeling/oliver_plots.sh
@@ -4,7 +4,7 @@
 cd ~/Desktop/summer_2021/icesat/data/raw/cuba
 
 # create list of threshold values to loop through
-declare -a threshold_array=("20" "25" "30" "35" "40" "45" "50" "55" "60" "65" "70" "75")
+declare -a threshold_array=(30 35 40 45 50 55 60 65 70 75)
 
 for filename in *.h5
 do
