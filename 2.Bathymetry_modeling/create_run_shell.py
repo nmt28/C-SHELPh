da920c9efbff691a771c2907ce0a70c265a5f350
Nathan Thomas <nmthomas28@gmail.com>
2022-02-18 11:24:28 -0500

    updated create_run_shell.py

--- 2.Bathymetry_modeling/create_run_shell.py
+++ 2.Bathymetry_modeling/create_run_shell.py
@@ -6,15 +6,15 @@ thresh_array = [30,35,40,45,50,55,60,65,70,75]
 
 laser_num = [1, 2, 3]
 
-dir = '/Users/nmthomas/Documents/Research/ICESat2/SharkBay_ATL03/'
+dir = 'dir to h5 files'
 
 files = glob.glob(dir + '/*.h5')
 
-txt_file = '/Users/nmthomas/Documents/Developer/Icesat2_bathymetry/4.Bathymetry_modeling/run_bathy.sh'
+txt_file = 'run_bathy.sh'
 txt = open(txt_file, 'w+')
 
 
 for file in files:
     for l in laser_num:
         for t in thresh_array:
-            txt.write('python /Users/nmthomas/Documents/Developer/Icesat2_bathymetry/2.Bathymetry_modeling/run_bathy.py -i ' + str(file) + ' -l ' + str(l) + ' -th ' + str(t) + '\n')
+            txt.write('python run_bathy.py -i ' + str(file) + ' -l ' + str(l) + ' -th ' + str(t) + '\n')
