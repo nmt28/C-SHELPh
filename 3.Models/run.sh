8a9b64976eb567a2d5ae3d879570db2232e5cd7e
Nathan Thomas <nmthomas28@gmail.com>
2022-02-18 11:42:43 -0500

    Added regression model

--- 3.Models/run.sh
+++
@@ -1,7 +0,0 @@
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/pete/Temp/rsgislib_v5_dev_install/lib
-export PYTHONPATH=$PYTHONPATH:/Users/pete/Temp/rsgislib_v5_dev_install/lib/python3.9/site-packages
-
-#python
-python run_bathy_analysis.py
-
-
--- Utilities/README.md
+++
@@ -1,1 +0,0 @@
-ICESat-2 depth data and Sentinel-2 image can be found here: https://drive.google.com/drive/folders/1dBI8DpvRzICjeQmLFaaMxppK9wd1llOA?usp=sharing
