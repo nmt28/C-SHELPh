b18c7c96c38e2e6643f156e2e80c2743b352dc50
nmt28 <nmthomas28@gmail.com>
2022-07-01 15:00:03 -0400

    revert of smoothing attempt

--- 2.Bathymetry_modeling/bathy_utils.py
+++ 2.Bathymetry_modeling/bathy_utils.py
@@ -154,15 +154,7 @@ def ref_linear_interp(x, y):
             arr.append(sub)
         else:
             sub = np.linspace(min, max, len(idx))
-            try:
-                bin = abs(sub[-2] - sub[-1])
-            except:
-                bin = 0
-            if min > max:
-                new = np.linspace(min,max+bin/2, len(idx))
-            elif max > min:
-                new = np.linspace(min+bin/2,max, len(idx))
-            arr.append(new)
+            arr.append(sub)
 
     return np.concatenate(arr, axis=None).ravel()
 
