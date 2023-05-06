82ff70f439cb0ab5b2e79b35d50b18508aca5d6e
Nathan Thomas <nmthoma1@gs618-nthomam2.fios-router.home>
2023-05-06 14:15:17 -0400

    final tweak ups

--- cshelph/run_bathy_extraction.py
+++ cshelph/run_bathy_extraction.py
@@ -34,9 +34,14 @@ import math
 import fiona
 import geopandas
 from datetime import datetime
-# Import functions from utils.py
-from bathy_utils import *
+import zarr
+import xarray
+import fsspec
+# This should work whether running cshelph from .py or if installed via pip
+import cshelph
 # need s3fs installed
+print("Packages imported")
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -77,16 +82,16 @@ def main():
 #         os._exit(1)
     
     print('''
-THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMpLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A pARTICULAR pURpOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COpYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.''')
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.''')
     
     # Read in the data
-    latitude, longitude, photon_h, conf, ref_elev, ref_azimuth, ph_index_beg, segment_id, alt_sc, seg_ph_count = read_atl03(args.input, args.laser)
+    latitude, longitude, photon_h, conf, ref_elev, ref_azimuth, ph_index_beg, segment_id, alt_sc, seg_ph_count = cshelph.read_atl03(args.input, args.laser)
     
     # Find the epsg code
-    epsg_code = convert_wgs_to_utm(latitude[0], longitude[0])
+    epsg_code = cshelph.convert_wgs_to_utm(latitude[0], longitude[0])
     epsg_num = int(epsg_code.split(':')[-1])
     # Orthometrically correct the data using the epsg code
-    lat_utm, lon_utm, photon_h = orthometric_correction(latitude, longitude, photon_h, epsg_code)
+    lat_utm, lon_utm, photon_h = cshelph.orthometric_correction(latitude, longitude, photon_h, epsg_code)
     # count number of photons in each segment: DEpRECATED
     #ph_num_per_seg = count_ph_per_seg(ph_index_beg, photon_h)
     ph_num_per_seg = seg_ph_count[ph_index_beg>0]
@@ -96,9 +101,9 @@ THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMp
     # count_ph_per_seg() function removes zeros from ph_index_beg
     # These 0s are nodata vals in other params (ref_elev etc)
     # Thus no pre-processing is needed as it will map correctly given the nodata values are eliminated
-    ph_ref_elev = ref_linear_interp(ph_num_per_seg, ref_elev[ph_index_beg>0])
-    ph_ref_azimuth = ref_linear_interp(ph_num_per_seg, ref_azimuth[ph_index_beg>0])
-    ph_sat_alt = ref_linear_interp(ph_num_per_seg, alt_sc[ph_index_beg>0])
+    ph_ref_elev = cshelph.ref_linear_interp(ph_num_per_seg, ref_elev[ph_index_beg>0])
+    ph_ref_azimuth = cshelph.ref_linear_interp(ph_num_per_seg, ref_azimuth[ph_index_beg>0])
+    ph_sat_alt = cshelph.ref_linear_interp(ph_num_per_seg, alt_sc[ph_index_beg>0])
 
     # Aggregate data into dataframe
     dataset_sea = pd.DataFrame({'latitude': lat_utm, 'longitude': lon_utm, 'photon_height': photon_h, 'confidence':conf, 'ref_elevation':ph_ref_elev, 'ref_azminuth':ph_ref_azimuth, 'ref_sat_alt':ph_sat_alt},
@@ -121,10 +126,10 @@ THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMp
     #plt.show()
     # Bin dataset
     print(dataset_sea1.head())
-    binned_data_sea = bin_data(dataset_sea1, args.lat_res, args.h_res)
+    binned_data_sea = cshelph.bin_data(dataset_sea1, args.lat_res, args.h_res)
     
     # Find mean sea height
-    sea_height = get_sea_height(binned_data_sea)
+    sea_height = cshelph.get_sea_height(binned_data_sea)
     
     # Set sea height
     med_water_surface_h = np.nanmedian(sea_height)
@@ -134,7 +139,7 @@ THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMp
         water_temp = args.water_temp
     else:
         try:
-            water_temp = get_water_temp(args.input, latitude, longitude)
+            water_temp = cshelph.get_water_temp(args.input, latitude, longitude)
         except Exception as e:
             print('NO SST pROVIDED OR RETRIEVED: 20 degrees C assigned')
             water_temp = 20
@@ -144,7 +149,7 @@ THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMp
 
     # Correct for refraction 
     print('refrac correction')
-    ref_x, ref_y, ref_z, ref_conf, raw_x, raw_y, raw_z, ph_ref_azi, ph_ref_elev = refraction_correction(water_temp, med_water_surface_h, 532, dataset_sea1.ref_elevation, dataset_sea1.ref_azminuth, dataset_sea1.photon_height, dataset_sea1.longitude, dataset_sea1.latitude, dataset_sea1.confidence, dataset_sea1.ref_sat_alt)
+    ref_x, ref_y, ref_z, ref_conf, raw_x, raw_y, raw_z, ph_ref_azi, ph_ref_elev = cshelph.refraction_correction(water_temp, med_water_surface_h, 532, dataset_sea1.ref_elevation, dataset_sea1.ref_azminuth, dataset_sea1.photon_height, dataset_sea1.longitude, dataset_sea1.latitude, dataset_sea1.confidence, dataset_sea1.ref_sat_alt)
     
     # Find bathy depth
     depth = med_water_surface_h - ref_z
@@ -159,23 +164,23 @@ THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMp
     #geodf.to_file(file + ".gpkg", driver="GpKG")
     
     # Bin dataset again for bathymetry
-    binned_data = bin_data(dataset_bath, args.lat_res, args.h_res)
+    binned_data = cshelph.bin_data(dataset_bath, args.lat_res, args.h_res)
     
     if isinstance(args.thresh, int) == True:
         # Find bathymetry
-        bath_height, geo_df = get_bath_height(binned_data, args.thresh, med_water_surface_h, args.h_res)
+        bath_height, geo_df = cshelph.get_bath_height(binned_data, args.thresh, med_water_surface_h, args.h_res)
         
         # Create figure
         plt.close()
-        produce_figures(binned_data, bath_height, sea_height, 10, -20, args.thresh, file, geo_df, ref_y, ref_z, args.laser, epsg_num)
+        cshelph.produce_figures(binned_data, bath_height, sea_height, 10, -20, args.thresh, file, geo_df, ref_y, ref_z, args.laser, epsg_num)
     elif isinstance(args.threshlist, list)==True:
         for thresh in args.threshlist:
             print("using threshold:", str(thresh))
-            bath_height, geo_df = get_bath_height(binned_data, int(thresh), med_water_surface_h, args.h_res)
+            bath_height, geo_df = cshelph.get_bath_height(binned_data, int(thresh), med_water_surface_h, args.h_res)
         
             # Create figure
             plt.close()
-            produce_figures(binned_data, bath_height, sea_height, 10, -20, str(thresh), file, geo_df, ref_y, ref_z, args.laser, epsg_num)
+            cshelph.produce_figures(binned_data, bath_height, sea_height, 10, -20, str(thresh), file, geo_df, ref_y, ref_z, args.laser, epsg_num)
 
     
 if __name__ == '__main__':
