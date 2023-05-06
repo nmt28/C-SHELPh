0a9d1dbcffe1bf6b81ba57b905df5d050086bf04
Nathan Thomas <nmthoma1@gs618-nthomam2.fios-router.home>
2023-05-06 12:00:50 -0400

    updated variable names to create cleaner code and removed redundant imports

--- 2.Bathymetry_modeling/bathy_utils.py
+++ 2.Bathymetry_modeling/bathy_utils.py
@@ -2,26 +2,24 @@
 # coding: utf-8
 
 '''
-THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'''
+THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF An_y KIND, EXpRESS OR IMpLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A pARTICULAR pURpOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COpYRIGHT HOLDERS BE LIABLE FOR An_y CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'''
 
 #####
-# This group of functions processes ICESAT2 data and creates a bathymetric model. 
+# This group of functions processes ICESAT2 data and creates a bathymetric model.
 # To do this, it follows a number of steps in the form of functions, including:
 # 1. Reading data (ReadATL03())
 # 2. Orthometrically correcting the dataset (OrthometricCorrection())
-# 3. Pulling down the data segment ID (getAtl03SegID())
+# 3. pulling down the data segment ID (getAtl03SegID())
 # 4. Bin the data along latitudinal and height gradients (bin_data())
 # 5. Calculate sea height (get_sea_height())
 # 6. Get water temperature (get_water_temp())
 # 7. Correct bathymetry surface for refraction (RefractionCorrection())
 # 8. Calculate bathymetry height (get_bath_height())
-# 9. Produce figures (produce_figures())
+# 9. produce figures (produce_figures())
 #####
 
 import numpy as np
 import h5py as h5
-from matplotlib.widgets import LassoSelector
-from matplotlib.path import Path
 import matplotlib.pyplot as plt
 import pyproj
 from pyproj import Proj
@@ -29,7 +27,6 @@ from pyproj import Transformer
 import pandas as pd
 import argparse
 import os
-import subprocess
 import time
 import utm
 import math
@@ -40,8 +37,9 @@ from datetime import datetime
 import utm
 import xarray as xr
 import fsspec
+# need s3fs installed
 
-def ReadATL03(h5_file, laser_num):
+def read_atl03(h5_file, laser_num):
     # Read File
     f = h5.File(h5_file,'r')
     
@@ -80,19 +78,19 @@ def convert_wgs_to_utm(lat, lon):
 		
 	return epsg
 
-def OrthometricCorrection(lat, lon, Z, epsg):
+def orthometric_correction(lat, lon, Z, epsg):
     # transform ellipsod (WGS84) height to orthometric height
     transformerh = Transformer.from_crs("epsg:4326", "epsg:3855", always_xy=True)
     X_egm08, Y_egm08, Z_egm08 = transformerh.transform(lon, lat, Z)
     
     # transform WGS84 proj to local UTM
-    myProj = Proj(epsg)
-    X_utm, Y_utm = myProj(lon, lat)
+    myproj = Proj(epsg)
+    X_utm, Y_utm = myproj(lon, lat)
     
     return Y_utm, X_utm, Z_egm08
 
     
-def count_ph_per_seg(ph_index_beg, photon_h): # DEPRECATED
+def count_ph_per_seg(ph_index_beg, photon_h): # DEpRECATED
     
     ph_index_beg = ph_index_beg[ph_index_beg!=0]
     
@@ -202,7 +200,7 @@ def get_sea_height(binned_data):
 
 def get_water_temp_redacted(data_path, latitude, longitude):
     '''
-    Pull down surface water temperature along the track from the JPL GHRSST opendap website.
+    pull down surface water temperature along the track from the JpL GHRSST opendap website.
     
     The GHRSST data are gridded tiles with dimension 17998 x 35999. 
     To get the specific grid tile of the SST, you must convert from lat, lon coordinates
@@ -237,10 +235,10 @@ def get_water_temp_redacted(data_path, latitude, longitude):
     new_lon_ratio = round(((lon_avg - lon_min) / (lon_max - lon_min)) *
                     (scaled_lon_max - scaled_lon_min) + lon_min)
 
-    # Access the SST data using the JPL OpenDap interface
-    url = 'https://opendap.jpl.nasa.gov/opendap/OceanTemperature/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/'\
+    # Access the SST data using the JpL OpenDap interface
+    url = 'https://opendap.jpl.nasa.gov/opendap/OceanTemperature/ghrsst/data/GDS2/L4/GLOB/JpL/MUR/v4.1/'\
         + str(year) + '/' + str(julian_day) + '/' + str(file_date) \
-        + '090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc'
+        + '090000-JpL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc'
     
     dataset = netCDF4.Dataset(url)
     
@@ -250,8 +248,8 @@ def get_water_temp_redacted(data_path, latitude, longitude):
 
 def get_water_temp(data_path, latitude, longitude):
     '''
-    Pull down surface water temperature along the track from the MUR SST:
-    https://aws.amazon.com/marketplace/pp/prodview-kvgy4vkuhavsc?sr=0-3&ref_=beagle&applicationId=AWSMPContessa#overview.
+    pull down surface water temperature along the track from the MUR SST:
+    https://aws.amazon.com/marketplace/pp/prodview-kvgy4vkuhavsc?sr=0-3&ref_=beagle&applicationId=AWSMpContessa#overview.
     '''
     # Get date from data filename
     file_date = data_path[-33:-25]
@@ -273,25 +271,22 @@ def get_water_temp(data_path, latitude, longitude):
     
     return sst
 
-def RefractionCorrection(WTemp, WSmodel, Wavelength, Photon_ref_elev, Ph_ref_azimuth, PhotonZ, PhotonX, PhotonY, Ph_Conf, satellite_altitude):
+def refraction_correction(water_temp, water_surface, wavelength, photon_ref_elev, ph_ref_azimuth, photon_z, photon_x, photon_y, ph_conf, satellite_altitude):
     
     '''
-    WTemp; there is python library that pulls water temp data 
-    WSmodel is the value surface height
+    WTemp; there is python library that pulls water temp data
+    water_surface is the value surface height
     Wavelength is fixed
     '''
     
     # Only process photons below water surface model
-    PhotonX = PhotonX[PhotonZ<=WSmodel]
-    PhotonY = PhotonY[PhotonZ<=WSmodel]
-    Photon_ref_elev = Photon_ref_elev[PhotonZ<=WSmodel]
-    satellite_altitude = satellite_altitude[PhotonZ<=WSmodel]
-    Ph_ref_azimuth = Ph_ref_azimuth[PhotonZ<=WSmodel]
-    Ph_Conf = Ph_Conf[PhotonZ<=WSmodel]
-    PhotonZ = PhotonZ[PhotonZ<=WSmodel]
-    
-    # water temp for refraction correction
-    WaterTemp= WTemp
+    photon_x = photon_x[photon_z<=water_surface]
+    photon_y = photon_y[photon_z<=water_surface]
+    photon_ref_elev = photon_ref_elev[photon_z<=water_surface]
+    satellite_altitude = satellite_altitude[photon_z<=water_surface]
+    ph_ref_azimuth = ph_ref_azimuth[photon_z<=water_surface]
+    ph_conf = ph_conf[photon_z<=water_surface]
+    photon_z = photon_z[photon_z<=water_surface]
     
     # Refraction coefficient #
     a = -0.000001501562500
@@ -299,23 +294,23 @@ def RefractionCorrection(WTemp, WSmodel, Wavelength, Photon_ref_elev, Ph_ref_azi
     c = -0.000042759374989
     d = -0.000160475520686
     e = 1.398067112092424
-    wl = Wavelength
+    wl = wavelength
     
     # refractive index of air
     n1 = 1.00029
     
     # refractive index of water
-    n2 = (a*WaterTemp**2) + (b*wl**2) + (c*WaterTemp) + (d*wl) + e
+    n2 = (a*water_temp**2) + (b*wl**2) + (c*water_temp) + (d*wl) + e
     
     # assumption is 0.25416
     # This example is refractionCoef = 0.25449
     # 1.00029 is refraction of air constant
-    CorrectionCoef = (1-(n1/n2))
+    #correction_coef = (1-(n1/n2))
     #########################
     
     # read photon ref_elev to get theta1
     # Does not account for curvature of Earth
-    theta1 = np.pi/2 - Photon_ref_elev
+    theta1 = np.pi/2 - photon_ref_elev
     
     # H = orbital altitude of IS2 (496km as mean)
     # H = 496km. we pass in the mean of the orbit from /geolocation/altitude_sc/
@@ -335,7 +330,7 @@ def RefractionCorrection(WTemp, WSmodel, Wavelength, Photon_ref_elev, Ph_ref_azi
     # eq 3. S
     # Approximate water Surface = 1.5
     # D  = raw uncorrected depth
-    D = WSmodel - PhotonZ
+    D = water_surface - photon_z
     
     # For Triangle DTS
     S = D/np.cos(theta1)
@@ -344,48 +339,36 @@ def RefractionCorrection(WTemp, WSmodel, Wavelength, Photon_ref_elev, Ph_ref_azi
     R = (S*n1)/n2
     Gamma = (np.pi/2)-theta1
     
-    # For triangle RPS
+    # For triangle RpS
     # phi is an angle needed
     phi = theta1-theta2
     
-    # P is the difference between raw and corrected YZ location
-    P = np.sqrt(R**2 + S**2 - 2*R*S*np.cos(phi))
+    # p is the difference between raw and corrected YZ location
+    p = np.sqrt(R**2 + S**2 - 2*R*S*np.cos(phi))
     
     # alpha is an angle needed
-    alpha = np.arcsin((R*np.sin(phi))/P)
+    alpha = np.arcsin((R*np.sin(phi))/p)
     
     # Beta angle needed for Delta Y an d Delta Z
     Beta = Gamma - alpha
     
     # Delta Y
-    DY = P*np.cos(Beta)
+    DY = p*np.cos(Beta)
     
     # Delta Z
-    DZ = P*np.sin(Beta)
+    DZ = p*np.sin(Beta)
     
     # Delta Easting
-    DE = DY*np.sin(Ph_ref_azimuth)
+    DE = DY*np.sin(ph_ref_azimuth)
     
     # Delta Northing
-    DN = DY*np.cos(Ph_ref_azimuth)
+    DN = DY*np.cos(ph_ref_azimuth)
     
-    outX = PhotonX + DE
-    outY = PhotonY + DN
-    outZ = PhotonZ + DZ
-    '''
-        print('\nFor selected Bathy photon:')
-        print('lat = ', PhotonY[9000])
-        print('long = ', PhotonX[9000])
-        print('Raw Depth = ', PhotonZ[9000])
-        print('D = ', D[9000])
-        
-        print('ref_elev = ', Photon_ref_elev[9000])
-        
-        print('Delta Easting = ', DE[9000])
-        print('Delta Northing = ', DN[9000])
-        print('Delta Z = ', DZ[9000])
-        '''
-    return(outX, outY, outZ, Ph_Conf, PhotonX, PhotonY, PhotonZ, Ph_ref_azimuth, Photon_ref_elev) # We are most interested in outx, outy, outz
+    out_x = photon_x + DE
+    out_y = photon_y + DN
+    out_z = photon_z + DZ
+
+    return(out_x, out_y, out_z, ph_conf, photon_x, photon_y, photon_z, ph_ref_azimuth, photon_ref_elev) # We are most interested in out_x, out_y, out_z
 
 def get_bath_height(binned_data, percentile, WSHeight, height_resolution):
     '''Calculate bathymetry level per bin based on horizontal resolution'''
@@ -398,7 +381,7 @@ def get_bath_height(binned_data, percentile, WSHeight, height_resolution):
     
     # Group data by latitude
     # Filter out surface data that are two bins below median surface value calculated above
-    binned_data_bath = binned_data[(binned_data['photon_height'] < WSHeight - (height_resolution * 2))] 
+    binned_data_bath = binned_data[(binned_data['photon_height'] < WSHeight - (height_resolution * 2))]
     grouped_data = binned_data_bath.groupby(['lat_bins'], group_keys=True)
     data_groups = dict(list(grouped_data))
     
@@ -454,21 +437,21 @@ def produce_figures(binned_data, bath_height, sea_height, y_limit_top, y_limit_b
     # Define figure size
     fig = plt.rcParams["figure.figsize"] = (40,5)
     
-    # Plot raw points
+    # plot raw points
     plt.scatter(x=binned_data.latitude, y = binned_data.photon_height, marker='o', lw=0, s=1, alpha = 0.8, c = 'yellow', label ='Raw photon height')
     plt.scatter(RefY, RefZ, s=0.2, alpha=0.1, c='black')
-    plt.scatter(geo_df.latitude, geo_df.photon_height, s=0.5, alpha=0.1, c='red', label = 'Classified Photons')
+    plt.scatter(geo_df.latitude, geo_df.photon_height, s=0.5, alpha=0.1, c='red', label = 'Classified photons')
     
     #plt.scatter(x=geo_df.latitude, y = geo_df.photon_height, marker='o', lw=0, s=0.8, alpha = 0.8, c = 'black', label = 'Corrected photon bin')
 
-    # Plot median values
+    # plot median values
     #plt.scatter(bath_median_df.x, bath_median_df.y, marker = 'o', c='r', alpha = 0.8, s = 5, label = 'Median bathymetry')
     plt.scatter(sea_median_df.x, sea_median_df.y, marker = 'o', c='b', alpha = 1, s = 0.5, label = 'Median sea surface')
     
     # Insert titles and sub-titles
     plt.title('Icesat2 Bathymetry\n' + file)
     plt.xlabel('Latitude', fontsize=25)
-    plt.ylabel('Photon Height (m)', fontsize=25)
+    plt.ylabel('photon Height (m)', fontsize=25)
     plt.xticks(fontsize=16)
     plt.yticks(fontsize=16)
 
@@ -482,12 +465,12 @@ def produce_figures(binned_data, bath_height, sea_height, y_limit_top, y_limit_b
     file = file.replace('.h5','')
     # Define where to save file
     plt.tight_layout()
-    plt.savefig(file + '_gt' + str(laser) + '_' + str(percentile) + '_EPSG' + str(epsg_num) + '_' + timestr + ".png")
+    plt.savefig(file + '_gt' + str(laser) + '_' + str(percentile) + '_EpSG' + str(epsg_num) + '_' + timestr + ".png")
     #plt.show()
     #plt.close()
         
         # convert corrected locations back to wgs84 (useful to contain)
-    transformer = Transformer.from_crs("EPSG:"+str(epsg_num), "EPSG:4326", always_xy=True)
+    transformer = Transformer.from_crs("EpSG:"+str(epsg_num), "EpSG:4326", always_xy=True)
     #print(transformer)
     lon_wgs84, lat_wgs84 = transformer.transform(geo_df.longitude.values, geo_df.latitude.values)
 
@@ -498,4 +481,4 @@ def produce_figures(binned_data, bath_height, sea_height, y_limit_top, y_limit_b
     
     geodf.set_crs(epsg=4326, inplace=True)
     
-    geodf.to_file(file + '_gt' + str(laser) + '_' + str(percentile) + '_EPSG' + str(epsg_num) + '_' + timestr + ".gpkg", driver="GPKG")
+    geodf.to_file(file + '_gt' + str(laser) + '_' + str(percentile) + '_EpSG' + str(epsg_num) + '_' + timestr + ".gpkg", driver="GPKG")
--- 2.Bathymetry_modeling/run_bathy.py
+++ 2.Bathymetry_modeling/run_bathy.py
@@ -2,27 +2,25 @@
 # coding: utf-8
 
 '''
-THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'''
+THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMpLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A pARTICULAR pURpOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COpYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'''
 
 
 #####
-# This group of functions processes ICESAT2 data and creates a bathymetric model. 
+# This group of functions processes ICESAT2 data and creates a bathymetric model.
 # To do this, it follows a number of steps in the form of functions, including:
 # 1. Reading data (ReadATL03())
 # 2. Orthometrically correcting the dataset (OrthometricCorrection())
-# 3. Pulling down the data segment ID (getAtl03SegID())
+# 3. pulling down the data segment ID (getAtl03SegID())
 # 4. Bin the data along latitudinal and height gradients (bin_data())
 # 5. Calculate sea height (get_sea_height())
 # 6. Get water temperature (get_water_temp())
 # 7. Correct bathymetry surface for refraction (RefractionCorrection())
 # 8. Calculate bathymetry height (get_bath_height())
-# 9. Produce figures (produce_figures())
+# 9. produce figures (produce_figures())
 #####
 
 import numpy as np
 import h5py as h5
-from matplotlib.widgets import LassoSelector
-from matplotlib.path import Path
 import matplotlib.pyplot as plt
 import pyproj
 from pyproj import Proj
@@ -30,7 +28,6 @@ from pyproj import Transformer
 import pandas as pd
 import argparse
 import os
-import subprocess
 import time
 import utm
 import math
@@ -39,6 +36,7 @@ import geopandas
 from datetime import datetime
 # Import functions from utils.py
 from bathy_utils import *
+# need s3fs installed
 
 def main():
     parser = argparse.ArgumentParser()
@@ -49,7 +47,7 @@ def main():
     parser.add_argument("-o", "--output", type=str, required = False, help="Specify the output location")
     parser.add_argument("-lr", "--lat_res", type=float, default = 10, help="Specify the latitudinal resoltuion (normally 10)")
     parser.add_argument("-hr", "--h_res", type=float, default = 0.5, help="Specify the height resolution (normally 0.5)")
-    parser.add_argument("-wt", "--waterTemp", type=float, default = None, required = False, help="Specify the water temperature in degrees C")
+    parser.add_argument("-wt", "--water_temp", type=float, default = None, required = False, help="Specify the water temperature in degrees C")
     parser.add_argument("-slat", "--start_lat", type=float, required = False, help="Specify the start latitude")
     parser.add_argument("-elat", "--end_lat", type=float, required = False, help="Specify the stop latitude")
     
@@ -61,8 +59,8 @@ def main():
     elif args.laser == None:
         print('MISSING LASER NUMBER')
         os._exit(1)
-    #elif args.waterTemp == None:
-    #    print('MISSING WATER TEMP')
+    #elif args.water_temp == None:
+    #    print('MISSING WATER TEMp')
     #    os._exit(1)
     elif args.lat_res == None:
         print('MISSING LATITUDINAL RESOLUTION')
@@ -72,42 +70,38 @@ def main():
         os._exit(1)
     elif args.thresh == None:
         if args.threshlist == None:
-            print('MISSING PERCENT THRESHOLD VALUE OR LIST')
+            print('MISSING pERCENT THRESHOLD VALUE OR LIST')
             os._exit(1)
 #     elif args.output == None:
-#         print('MISSING OUTPUT DIRECTORY')
+#         print('MISSING OUTpUT DIRECTORY')
 #         os._exit(1)
     
     print('''
-THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.''')
+THE SOFTWARE IS pROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXpRESS OR IMpLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A pARTICULAR pURpOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COpYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.''')
     
     # Read in the data
-    latitude, longitude, photon_h, conf, ref_elev, ref_azimuth, ph_index_beg, segment_id, alt_sc, seg_ph_count = ReadATL03(args.input, args.laser)
+    latitude, longitude, photon_h, conf, ref_elev, ref_azimuth, ph_index_beg, segment_id, alt_sc, seg_ph_count = read_atl03(args.input, args.laser)
     
     # Find the epsg code
     epsg_code = convert_wgs_to_utm(latitude[0], longitude[0])
     epsg_num = int(epsg_code.split(':')[-1])
     # Orthometrically correct the data using the epsg code
-    lat_utm, lon_utm, photon_h = OrthometricCorrection(latitude, longitude, photon_h, epsg_code)
-    # count number of photons in each segment: DEPRECATED
-    #Ph_num_per_seg = count_ph_per_seg(ph_index_beg, photon_h)
-    Ph_num_per_seg = seg_ph_count[ph_index_beg>0]
+    lat_utm, lon_utm, photon_h = orthometric_correction(latitude, longitude, photon_h, epsg_code)
+    # count number of photons in each segment: DEpRECATED
+    #ph_num_per_seg = count_ph_per_seg(ph_index_beg, photon_h)
+    ph_num_per_seg = seg_ph_count[ph_index_beg>0]
     # Cast as an int
-    Ph_num_per_seg = Ph_num_per_seg.astype(np.int64)
+    ph_num_per_seg = ph_num_per_seg.astype(np.int64)
         
     # count_ph_per_seg() function removes zeros from ph_index_beg
     # These 0s are nodata vals in other params (ref_elev etc)
     # Thus no pre-processing is needed as it will map correctly given the nodata values are eliminated
-    Ph_ref_elev = ref_linear_interp(Ph_num_per_seg, ref_elev[ph_index_beg>0])
-    Ph_ref_azimuth = ref_linear_interp(Ph_num_per_seg, ref_azimuth[ph_index_beg>0])
-    Ph_sat_alt = ref_linear_interp(Ph_num_per_seg, alt_sc[ph_index_beg>0]) 
-    
-    #plt.scatter(np.arange(1, len(Ph_ref_azimuth)+1, 1), Ph_ref_azimuth)
-    #plt.show()
-    
-    
+    ph_ref_elev = ref_linear_interp(ph_num_per_seg, ref_elev[ph_index_beg>0])
+    ph_ref_azimuth = ref_linear_interp(ph_num_per_seg, ref_azimuth[ph_index_beg>0])
+    ph_sat_alt = ref_linear_interp(ph_num_per_seg, alt_sc[ph_index_beg>0])
+
     # Aggregate data into dataframe
-    dataset_sea = pd.DataFrame({'latitude': lat_utm, 'longitude': lon_utm, 'photon_height': photon_h, 'confidence':conf, 'ref_elevation':Ph_ref_elev, 'ref_azminuth':Ph_ref_azimuth, 'ref_sat_alt':Ph_sat_alt}, 
+    dataset_sea = pd.DataFrame({'latitude': lat_utm, 'longitude': lon_utm, 'photon_height': photon_h, 'confidence':conf, 'ref_elevation':ph_ref_elev, 'ref_azminuth':ph_ref_azimuth, 'ref_sat_alt':ph_sat_alt},
                            columns=['latitude', 'longitude', 'photon_height', 'confidence', 'ref_elevation', 'ref_azminuth', 'ref_sat_alt'])
     
     #plt.scatter(lat_utm, photon_h, c='black', s=0.1, alpha=0.1)
@@ -133,62 +127,55 @@ THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMP
     sea_height = get_sea_height(binned_data_sea)
     
     # Set sea height
-    WSHeight = np.nanmedian(sea_height)
+    med_water_surface_h = np.nanmedian(sea_height)
 
     # Calculate sea temperature
-    if args.waterTemp is not None:
-        waterTemp = args.waterTemp
+    if args.water_temp is not None:
+        water_temp = args.water_temp
     else:
         try:
-            waterTemp = get_water_temp(args.input, latitude, longitude)
+            water_temp = get_water_temp(args.input, latitude, longitude)
         except Exception as e:
-            print('NO SST PROVIDED OR RETRIEVED: 20 degrees C assigned')
-            waterTemp = 20 
+            print('NO SST pROVIDED OR RETRIEVED: 20 degrees C assigned')
+            water_temp = 20
     
-    print("water temp:", waterTemp)
-    
-    #except OSError:
-    #    sst_calculated = 20
-        
-    #if 15 <= sst_calculated <= 30:
-    #    waterTemp = sst_calculated
-    #else:
-    #    waterTemp = 20
+    print("water temp:", water_temp)
+
 
     # Correct for refraction 
     print('refrac correction')
-    RefX, RefY, RefZ, RefConf, rawX, rawY, rawZ, ph_ref_azi, ph_ref_elev = RefractionCorrection(waterTemp, WSHeight, 532, dataset_sea1.ref_elevation, dataset_sea1.ref_azminuth, dataset_sea1.photon_height, dataset_sea1.longitude, dataset_sea1.latitude, dataset_sea1.confidence, dataset_sea1.ref_sat_alt)
+    ref_x, ref_y, ref_z, ref_conf, raw_x, raw_y, raw_z, ph_ref_azi, ph_ref_elev = refraction_correction(water_temp, med_water_surface_h, 532, dataset_sea1.ref_elevation, dataset_sea1.ref_azminuth, dataset_sea1.photon_height, dataset_sea1.longitude, dataset_sea1.latitude, dataset_sea1.confidence, dataset_sea1.ref_sat_alt)
     
     # Find bathy depth
-    depth = WSHeight - RefZ
+    depth = med_water_surface_h - ref_z
 
     # Create new dataframe with refraction corrected data
-    dataset_bath = pd.DataFrame({'latitude': rawY, 'longitude': rawX, 'cor_latitude':RefY, 'cor_longitude':RefX, 'cor_photon_height':RefZ, 'photon_height': rawZ, 'confidence':RefConf, 'depth':depth}, 
+    dataset_bath = pd.DataFrame({'latitude': raw_y, 'longitude': raw_x, 'cor_latitude':ref_y, 'cor_longitude':ref_x, 'cor_photon_height':ref_z, 'photon_height': raw_z, 'confidence':ref_conf, 'depth':depth},
                        columns=['latitude', 'longitude', 'photon_height', 'cor_latitude','cor_longitude', 'cor_photon_height', 'confidence', 'depth'])
 
      # Export dataframe to gpkg
     #geodf = geopandas.GeoDataFrame(dataset_bath, geometry=geopandas.points_from_xy(dataset_bath.longitude, dataset_bath.latitude))
     file = args.input
-    #geodf.to_file(file + ".gpkg", driver="GPKG")
+    #geodf.to_file(file + ".gpkg", driver="GpKG")
     
     # Bin dataset again for bathymetry
     binned_data = bin_data(dataset_bath, args.lat_res, args.h_res)
     
     if isinstance(args.thresh, int) == True:
         # Find bathymetry
-        bath_height, geo_df = get_bath_height(binned_data, args.thresh, WSHeight, args.h_res)
+        bath_height, geo_df = get_bath_height(binned_data, args.thresh, med_water_surface_h, args.h_res)
         
         # Create figure
         plt.close()
-        produce_figures(binned_data, bath_height, sea_height, 10, -20, args.thresh, file, geo_df, RefY, RefZ, args.laser, epsg_num)
+        produce_figures(binned_data, bath_height, sea_height, 10, -20, args.thresh, file, geo_df, ref_y, ref_z, args.laser, epsg_num)
     elif isinstance(args.threshlist, list)==True:
         for thresh in args.threshlist:
             print("using threshold:", str(thresh))
-            bath_height, geo_df = get_bath_height(binned_data, int(thresh), WSHeight, args.h_res)
+            bath_height, geo_df = get_bath_height(binned_data, int(thresh), med_water_surface_h, args.h_res)
         
             # Create figure
             plt.close()
-            produce_figures(binned_data, bath_height, sea_height, 10, -20, str(thresh), file, geo_df, RefY, RefZ, args.laser, epsg_num)
+            produce_figures(binned_data, bath_height, sea_height, 10, -20, str(thresh), file, geo_df, ref_y, ref_z, args.laser, epsg_num)
 
     
 if __name__ == '__main__':
